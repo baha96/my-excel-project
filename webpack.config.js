f35ddfc5a3c42e5b344e84fcef15ae75df476e3f
baha96 <87002133101a@gmail.com>
2020-05-19 14:44:19 +0600

    fix

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLwebPlagin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filename = (exp) => isDev ? `bundle.${exp}` : `bundle.[hash].${exp}`;
+const jsLoader = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  ];
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+  return loaders;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  // @babel/polyfill - это решить проблему ReferenceError: regeneratorRuntime is not defined
+  entry: ['@babel/polyfill', './index.js'],
+  devtool: isDev ? 'source-map' : false,
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: 8600,
+    hot: isDev
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLwebPlagin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoader(),
+      }
+    ],
+  }
+};
