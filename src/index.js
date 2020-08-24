7c09e0bb3925571f1b5223d599f42e9bdf07f0a0
baha96 <87002133101a@gmail.com>
2020-08-24 19:19:48 +0600

    fix comment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './scss/index.scss';
+import './assets/scss/index.scss';
 import {Router} from '@core/router/Router';
 import {DashboardPage} from '@/pages/DashboardPage';
 import {ExcelPage} from '@/pages/ExcelPage';
@@ -8,14 +8,4 @@ new Router('#app', {
   dashboard: DashboardPage,
   excel: ExcelPage,
 });
-// import {Excel} from '@/components/excel/Excel';
-// import {Header} from '@/components/header/Header';
-// import {Toolbar} from '@/components/toolbar/Toolbar';
-// import {Formula} from '@/components/formula/Formula';
-// import {Table} from '@/components/table/Table';
-// import {rootReducer} from '@/store/rootReducer';
-// import {CreateStore} from '@core/CreateStore';
-// import {storage, debounce} from '@core/utils';
-// import {initialState} from '@/store/initialState';
-//
 
